bf62f960ccd4ec18b37de73bc30cf3f4e22f2fb4
ffossum <fredrik.fossum@gmail.com>
2026-01-01 18:37:33 +0100

    new control scheme. movement is now relative to camera

--- client/player.gd
+++ client/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
+const TURN_SPEED = 15.0
 const JUMP_VELOCITY = 4.5
 
-@onready var _camera: Camera3D = %Camera3D
 @onready var _camera_pivot: Node3D = %CameraPivot
-@onready var _spring_arm_3d: SpringArm3D = %SpringArm3D
+@onready var _camera_arm: SpringArm3D = %CameraArm
 
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.005
 @export var tilt_limit = deg_to_rad(75)
@@ -13,10 +13,10 @@ const JUMP_VELOCITY = 4.5
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	var left_held := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
 	var right_held := Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
 	
@@ -25,18 +25,20 @@ func _physics_process(delta: float) -> void:
 	if left_held and right_held:
 		input_dir.y -= 1
 	
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if input_dir:
+		var direction := (_camera_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		rig.look_at(rig.global_position + direction, Vector3.UP, true)
+		if not right_held:
+			var target_yaw := atan2(direction.x, direction.z)
+			rotation.y = rotate_toward(rotation.y, target_yaw, TURN_SPEED * delta)
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		rig.rotation = Vector3.ZERO
-
-	move_and_slide()
 	
+	move_and_slide()
+
 @onready var animation_player: AnimationPlayer = $AnimationLibrary_Godot_Standard/AnimationPlayer
 @onready var rig: Node3D = $AnimationLibrary_Godot_Standard/Rig
 
@@ -64,27 +66,24 @@ func _unhandled_input(event: InputEvent) -> void:
 		if captured_mouse_position:
 			Input.warp_mouse(captured_mouse_position)
 			captured_mouse_position = Vector2.ZERO
-			
-
+	
 	if event is InputEventMouseMotion:
-		# Vertical rotation (pitch)
 		if left_held or right_held:
-			_camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
-			_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
-		
-		# Horizontal rotation (yaw)
-		if left_held and not right_held:
+			# Vertical rotation by tilting arm up/down
+			_camera_arm.rotation.x -= event.relative.y * mouse_sensitivity
+			_camera_arm.rotation.x = clampf(_camera_arm.rotation.x, -tilt_limit, tilt_limit)
+			
+			# Horizontal rotation by rotating the pivot
 			_camera_pivot.rotation.y += -event.relative.x * mouse_sensitivity
-		elif right_held:
+		
+		if right_held and not left_held:
 			rotation.y += -event.relative.x * mouse_sensitivity
-
+		
 	elif event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
-			_spring_arm_3d.spring_length -= 1.0
-			_spring_arm_3d.spring_length = max(_spring_arm_3d.spring_length, 1.0)
+			_camera_arm.spring_length -= 1.0
+			_camera_arm.spring_length = max(_camera_arm.spring_length, 1.0)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
-			_spring_arm_3d.spring_length += 1.0
+			_camera_arm.spring_length += 1.0
 		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			global_rotation.y = _camera_pivot.global_rotation.y
-			_camera_pivot.rotation.y = 0.0
-		
+			rotation.y = _camera_pivot.rotation.y + PI
